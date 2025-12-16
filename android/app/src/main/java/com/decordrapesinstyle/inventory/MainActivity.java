1e4b0893e775bb720c7c5810d9d0ddb8ada4930b
Aditya <decordrapes.instyle@gmail.com>
2025-12-16 17:13:09 +0530

    v4.1

--- android/app/src/main/java/com/decordrapesinstyle/inventory/MainActivity.java
+++ android/app/src/main/java/com/decordrapesinstyle/inventory/MainActivity.java
@@ -1,5 +1,15 @@
 package com.decordrapesinstyle.inventory;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Allow Android to control status bar correctly
+    getWindow().setStatusBarColor(0xFF000000);
+  }
+}
